34eba8dd01761aa4b52e66919f86e2abfec3323b
David Bernard <david.bernard.31@gmail.com>
2016-08-23 21:56:23 +0200

    remove tools/cnv_google_protobuf.sh, useless with proto3

--- tools/cnv_google_protobuf.sh
+++
@@ -1,8 +0,0 @@
-# more info at http://python3porting.com/bookindex.html
-# download protobuf-2.6.1.tar.gz for python
-cd $(dirname $0)/../modules
-rm -Rf google
-tar -xzvf ~/Téléchargements/protobuf-2.6.1.tar.gz protobuf-2.6.1/google
-2to3-3.4 protobuf-2.6.1/google -o google -W --no-diffs -p -n
-rm -Rf protobuf-2.6.1
-#find . -name '*.py' -type f -exec sed -i -e 's/except \([A-Za-z0-9_\.]*\), e/except \1 as e/g'  {} \;
